91591cfb414d91afef1ada096faba47fedf6cf40
igndukwe <ifeanyigloryndukwe@gmail.com>
2020-08-28 06:02:05 +1200

    Added Cart and Checkout view and templates

--- app/store/models.py
+++ app/store/models.py
@@ -67,6 +67,30 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        """Total amount of ordered items in cart"""
+
+        # Total is a product of price and quantity
+        # >OrderItem is a Child of Order
+        # Syntax: Parent.child_set.all()
+        # remember that child must be lower cases
+        # e.g. call from inside inside class/method:
+        # self.orderitem_set.all()
+        # call from outside the class:
+        # Order.orderitem_set.all()
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        """Total quantity of ordered items in cart"""
+
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     # on deleting OrderItem, do not delete products or orders
@@ -74,6 +98,14 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        """Total of a particular item"""
+
+        # Total is a product of price and quantity
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
--- app/store/views.py
+++ app/store/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 
 # Create your views here.
 def store(request):
     # query products
     products = Product.objects.all()
-    print(products)
+    # print(products)
     context = {'products': products}
     return render(
         request,
@@ -16,7 +16,31 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # query Order if exists otherwise create Order obj
+        # >complete=False: get the open orders,
+        # i.e. orders that have complete status of false
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
+
+        # Then get the items attached to that order
+        # To query child object from parent
+        # >child object must all be lower cases
+        # Syntax: parent.child_function
+        # e.g. OrderIdems is a child of Order
+        # Hence, to get all the order items that has an order
+        items = order.orderitem_set.all()
+    else:
+        # if user is not authenticated return an empty list
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+
     return render(
         request,
         'store/cart.html',
@@ -25,7 +49,20 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+
+    context = {'items': items, 'order': order}
     return render(
         request,
         'store/checkout.html',
